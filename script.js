0392a06e3f08e9481ba8473fb7a0125bffb90829
Kenneth <kenneth.kristopher27@gmail.com>
2022-05-12 18:50:41 +0700

    added navbar

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav-links');
+const header = document.querySelector('.header-container');
 
 hamburger.addEventListener('click', ()=>{
     nav.classList.toggle('nav-links-active');
     if(nav.classList.contains('nav-links-active')){
         hamburger.setAttribute('src', 'assets/icons/close.svg');
+        header.style.cssText = 'filter: brightness(30%);';
     } else {
         hamburger.setAttribute('src', 'assets/icons/hamburger.svg');
+        header.style.cssText = 'filter: brightness(100%);';
     }
 })
